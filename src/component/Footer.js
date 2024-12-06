6fa7270e988fad7dc3d7fbda68965adc13cbe8ef
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2024-12-06 17:44:25 +0530

    3 Commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -23,11 +23,36 @@ function Footer(){
                                 <i className="fa fa-facebook" aria-hidden="true"></i>
                             </a>
                         </li>
-                        
+                        <li className="wow fadeIn" data-wow-delay=".4s">
+                            <a href="" target="newtab">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li className="wow fadeIn" data-wow-delay="1s">
+                            <a href="" target="newtab">
+                                <i className="fa fa-medium" aria-hidden="true"></i>
+                            </a>
+                        </li>
                     </ul>
                 </div>
+                <div className="copyright">
+                    <h3>&copy; {currYear}, Made with by Pabodha Gamage</h3>
+                </div>
+                <a 
+                href=" "
+                title=""
+                class ="dmc-badge">
+
+                <img
+                src=""
+                alt />
+               </a>
+
+               <script src=""></script>
+
             </div>
         </React.StrictMode>
 
-    )
+    );
 }
+export default Footer;
