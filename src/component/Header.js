7d569be5c41eb0f5c36849f52c31dc4d9f4c2b24
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2024-12-10 14:57:40 +0530

    6 Commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -34,6 +34,29 @@ function Haeder(){
                                     id="bs-example-navbar-collapse-1"
                                     >
                                         <ul className="nav navbar-nav"></ul>
+                                        <li>
+                                            <a href="/#home" className="active">
+                                            Home
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/#about">About</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#skil">Skil</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#resum">Education</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#service">Service</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#blog">Blog</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#contact">Contact</a>
+                                        </li>
                                     </div>
                                 </div>
                             </div>
@@ -44,4 +67,7 @@ function Haeder(){
             </div>
         </React.StrictMode>
     )
+
 }
+
+export default Header;
