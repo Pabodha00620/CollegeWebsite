42d88278de85a671f21313ca368a4520630d6247
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2024-12-14 22:41:12 +0530

    8 commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <React className = "main-page-content">
+            <div id='home'>
+                <div id="particles-js"></div>
+                <div className="table-cell">
+                    <div className='container'>
+                        <div className='row home-row'>
+                            <div className='col-md-12 col-sm-12'>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React>
+    )
+}
