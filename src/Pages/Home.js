f6d2a8027267a03b907027d283573afe971479d0
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2025-02-19 13:08:45 +0530

    25 Commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -366,6 +366,37 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
+                    <div className='row main-row wow fadeIn'>
+                        <div className='col-md-12'>
+                            <div className='main-title text-center'>
+                                <h3>Working Experience</h3>
+                                <div className='underline1'></div>
+                                <div className='underline2'></div>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row love-row'>
+                        <div className='col-md-4 col-sm-12'>
+                            <div className='exp-details' data-wow-delay=".2s">
+                                <div className='exp-hover'></div>
+                                <div className='exp-main'>
+                                    <i
+                                    className='fa fa-building exp-icon'
+                                    aria-hidden="true"></i>
+                                    <h3>
+                                        <a
+                                        href=''
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        style={{color:"#4d4d4d"}}>
+                                            Patel G Tech LLP
+                                        </a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
